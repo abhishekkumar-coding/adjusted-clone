d1cf4516d3536e9ba2e23680a4753420fe65d0a8
abhishekkumar-coding <abhiwebdev2.0@gmail.com>
2024-12-11 13:17:20 +0530

    Replaced vanilla CSS with Tailwind CSS

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -5,17 +5,38 @@ import { NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div className='nyaheader'>
-            <div className='inside-nyaheader'>
-                <NavLink to="/" className="logo">
-                    <img src={logo} alt="website_logo" />
+        <div className='nyaheader bg-white w-[100%] flex item-center fixed top-0 left-0 z-50 shadow-lg'>
+            <div className='inside-nyaheader w-[100%] flex justify-between py-[0.5%] px-[16%]'>
+                <NavLink to="/" className="logo ">
+                    <img src={logo} alt="website_logo" style={{ width: '100px', height: '45px' }} />
                 </NavLink>
-                <div className='navbar'>
-                    <NavLink to="/careers">Careers</NavLink>
-                    <NavLink to="/events">Events</NavLink>
-                    <NavLink to="/csrpage">CSR</NavLink>
-                    <NavLink to="/contactus">Contacts</NavLink>
+                <div className="navbar flex items-center space-x-[70px]">
+                    <NavLink
+                        to="/careers"
+                        className="text-[15px] font-medium cursor-pointer transition-all duration-300 text-black no-underline hover:text-[#292B5B] hover:scale-[1.1]"
+                    >
+                        Careers
+                    </NavLink>
+                    <NavLink
+                        to="/events"
+                        className="text-[15px] font-medium cursor-pointer transition-all duration-300 text-black no-underline hover:text-[#292B5B] hover:scale-[1.1]"
+                    >
+                        Events
+                    </NavLink>
+                    <NavLink
+                        to="/csrpage"
+                        className="text-[15px] font-medium cursor-pointer transition-all duration-300 text-black no-underline hover:text-[#292B5B] hover:scale-[1.1]"
+                    >
+                        CSR
+                    </NavLink>
+                    <NavLink
+                        to="/contactus"
+                        className="text-[15px] font-medium cursor-pointer transition-all duration-300 text-black no-underline hover:text-[#292B5B] hover:scale-[1.1]"
+                    >
+                        Contacts
+                    </NavLink>
                 </div>
+
             </div>
         </div>
     )

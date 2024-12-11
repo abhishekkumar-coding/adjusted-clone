f7ef081335fb97460b47a1ddbdacdf07af368594
abhishekkumar-coding <abhiwebdev2.0@gmail.com>
2024-12-11 16:20:38 +0530

    Updated Hero section

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -37,43 +37,43 @@ const Home = () => {
     }, []);
 
     return (
-        <div className='homecontainer'>
-            <img className='compass' src={compass} alt='compass' />
-            <div className='upsideheader'>
+        <div className='homecontainer w-[100%] bg-[#F3F3F3] relative'>
+            <img className='compass absolute w-[250px] h-[450px] top-[1%] left-[-10px]' src={compass} alt='compass' />
+            <div className='upsideheader py-[1%] pl-[16%] pr-[14%]'>
                 {!showHeader && (
                     <div>
-                        <div className='insideheader'>
-                            <NavLink to="/aboutus" className='btn1'>About Us</NavLink>
-                            <img src={logo} alt='logo' />
-                            <NavLink to="/product" className='btn2'>Our Product</NavLink>
+                        <div className='insideheader flex items-center justify-between'>
+                            <NavLink to="/aboutus" className='btn1 py-[0.5%] px-[1.8%] text-[15px] text-[#000] font-[500] border-2 border-[#292b5b] rounded cursor-pointer transition-all duration-[0.2s] decoration-0 hover:bg-[#292b5b] hover:text-[#fff]'>About Us</NavLink>
+                            <img src={logo} alt='logo' style={{ width: "180px", height: "70px" }} />
+                            <NavLink to="/product" className='btn1 py-[0.5%] px-[1.8%] text-[15px] text-[#000] font-[500] border-2 border-[#292b5b] rounded cursor-pointer transition-all duration-[0.2s] decoration-0 hover:bg-[#292b5b] hover:text-[#fff]'>Our Product</NavLink>
                         </div>
-                        <div className='lists'>
-                            <NavLink to="/careers">Careers</NavLink>
-                            <NavLink to="/events">Events</NavLink>
-                            <NavLink to="/csrpage">CSR</NavLink>
-                            <NavLink to="/contactus">Contacts</NavLink>
+                        <div className='lists flex justify-center items-center ml-[6.5%]'>
+                            <NavLink className="decoration-0 text-[15px] cursor-pointer text-[#000] font-[500] mr-[8%] transition-all duration-[0.3s] mt-[1.5%] mb-[1%] hover:text-[#292b5b] hover:scale-[1.1]" to="/careers">Careers</NavLink>
+                            <NavLink className="decoration-0 text-[15px] cursor-pointer text-[#000] font-[500] mr-[8%] transition-all duration-[0.3s] mt-[1.5%] mb-[1%] hover:text-[#292b5b] hover:scale-[1.1]" to="/events">Events</NavLink>
+                            <NavLink className="decoration-0 text-[15px] cursor-pointer text-[#000] font-[500] mr-[8%] transition-all duration-[0.3s] mt-[1.5%] mb-[1%] hover:text-[#292b5b] hover:scale-[1.1]" to="/csrpage">CSR</NavLink>
+                            <NavLink className="decoration-0 text-[15px] cursor-pointer text-[#000] font-[500] mr-[8%] transition-all duration-[0.3s] mt-[1.5%] mb-[1%] hover:text-[#292b5b] hover:scale-[1.1]" to="/contactus">Contacts</NavLink>
                         </div>
                     </div>
                 )}
                 {/* Sticky Header visible on scroll */}
                 {showHeader && <Header />}
-                <div className='headertext'>
-                    <img className='plane' src={plane} alt='plane' />
-                    <img className='choosemore' src={headertext} alt='headertext' />
+                <div className='headertext flex items-center justify-center ml-[-12%] relative'>
+                    <img className='plane absolute top-0 w-[8%] h-[48px]' src={plane} alt='plane' />
+                    <img className='choosemore w-[55%] h-[30%]' src={headertext} alt='headertext' />
                 </div>
             </div>
-            <div className='whoweare'>
-                <img className='compass2nd' src={compass2} alt='compass' />
-                <h2>Who<span> We are </span></h2>
-                <div className='inside_whoweare'>
-                    <img src={WhoWeare} alt='WhoWeare' />
+            <div className='whoweare py-[1%] pl-[16%] pr-[14%] relative text-center '>
+                <img className='compass2nd absolute w-[220px] h-[450px] right-0 top-[-15px]' src={compass2} alt='compass' />
+                <h2 className='inline-block pb-[10px] text-[#000] font-[700] relative'>Who<span> We are </span></h2>
+                <div className='inside_whoweare flex justify-between items-center mt-[3%]'>
+                    <img src={WhoWeare} style={{ width: "450px", marginRight: "8%" }} alt='WhoWeare' />
                     <div>
-                        <p>FBT ADVENTURES is the leading online travel content aggregator and  innovator of Direct Connect distribution solutions. FBT ADVENTURES  operates the world’s largest Direct Connect distribution and payment  platform, directly linking hundreds of travel suppliers to Online Travel Agencies, Online (Corporate) Booking Tools, Travel Search and Mobile  services.  FBT team itself a bunch of passionate Travellers who started  this one unified aim, which is to make travelling easy for worldwide.</p>
+                        <p className='text-right font-[500] leading-6'>FBT ADVENTURES is the leading online travel content aggregator and  innovator of Direct Connect distribution solutions. FBT ADVENTURES  operates the world’s largest Direct Connect distribution and payment  platform, directly linking hundreds of travel suppliers to Online Travel Agencies, Online (Corporate) Booking Tools, Travel Search and Mobile  services.  FBT team itself a bunch of passionate Travellers who started  this one unified aim, which is to make travelling easy for worldwide.</p>
                     </div>
                 </div>
             </div>
             <div className="ourpride">
-                <img className="compass3" src={compass} alt="compass" />
+                <img className="compass3 absolute left-[-10px] w-[250px] h-[450px] top-0" src={compass} alt="compass" />
                 <h2>
                     What We<span> Pride Ourselves On</span>
                 </h2>
@@ -120,7 +120,7 @@ const Home = () => {
                 </div>
             </div>
             <div className='outpledge'>
-                <img className='compass4nd' src={compass2} alt='compass' />
+                <img className='compass4nd absolute w-[220px] h-[450px] right-0 top-[30%]' src={compass2} alt='compass' />
                 <div className='insidepledge'>
                     <p className='lowcost'>Low Cost Business Models</p>
                     <p className='direct'>Direct Access to Airlines,<br /> Bus and Hotel Supplier,<br /> Umrah, Visa, Holiday</p>
@@ -131,7 +131,7 @@ const Home = () => {
                 </div>
             </div>
             <div className='ourhonour'>
-                <img className='compass5' src={compass} alt='compass' />
+                <img className='compass5 absolute w-[250px] h-[450px] top-[15%] left-[-10px]' src={compass} alt='compass' />
                 <h2>Our<span> Honour </span></h2>
                 <div className="honour-grid">
                     <div className="honour-card">
@@ -157,7 +157,7 @@ const Home = () => {
                 </div>
             </div>
             <div className='ourvalue'>
-                <img className='compass6nd' src={compass2} alt='compass' />
+                <img className='compass6nd absolute w-[220px] h-[450px] right-0 top-[60%]' src={compass2} alt='compass' />
                 <div className='insidevalue'>
                     <img src={value} alt='value' />
                 </div>
